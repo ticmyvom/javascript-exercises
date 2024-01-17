8ba328bfcd6ae2a7475388c5af091265cf39ad49
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-01-16 16:43:32 -0800

    Finished the first 2 exercised

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // repeat `string` `num` times
+    /**
+     * Pseudocode:
+     * for loop from 0 to num, append string to the final result 
+     * return result
+     */
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let result = '';
+        for (let i = 0; i < num; i++) {
+            if (!string){ // if string is empty, return right away
+                return '';
+            }
+            result += string;
+        }
+        return result;
+    }
+    
 
 };
 
