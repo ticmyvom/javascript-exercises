a454821313329136372a376bbd6982f61db9f879
Anh Ngo <63425188+ticmyvom@users.noreply.github.com>
2024-02-11 11:30:13 -0800

    Add my solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().split('');
+    let alphanumStrArray = [];
+    str.forEach((char) => {
+        if (isAlphanumeric(char)) {
+            alphanumStrArray.push(char);
+        }
+    });
+    // alphanumStrArray should now contains alphanumeric characters only from str
 
+    // let leftIndex = 0,
+    //     rightIndex = alphanumStrArray.length - 1 - leftIndex;
+    // while (leftIndex <= rightIndex) {
+    //     if (alphanumStrArray[leftIndex] !== alphanumStrArray[rightIndex]) {
+    //         return false;
+    //     }
+    //     leftIndex++;
+    //     rightIndex--;
+    // }
+    // return true;
+    let alphanumStr = alphanumStrArray.join('');
+    return alphanumStrArray.reverse().join('') === alphanumStr;
 };
+
+function isAlphanumeric(char) {
+    return /\w+/.test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
